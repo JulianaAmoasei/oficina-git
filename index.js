cbf51f5b99158aebafb11203bb0a49c428e9dbd3
Juliana Amoasei <juliana.amoasei@gmail.com>
2022-01-19 14:38:02 -0300

    commit arquivos iniciais

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log('xuxu')
+
+/*
+FLUXO INICIAL (COMEÇANDO O PROJETO DO ZERO AGORA NESSE MOMENTO SEM FORKAR DE NENHUM OUTRO)
+0 - criar o repositório do projeto em github.com/seuperfil
+1 - navegar pra pasta certa
+2 - fizemos git init SÓ DA PRIMEIRA VEZ PRA CRIAR UM REPOSITÓRIO
+3 - fazer a comunicação entre o remoto (github) e o local (compiuter) com git remote add origin git@github.com:JulianaAmoasei/oficina-git.git
+4 - 
+
+
+*/
